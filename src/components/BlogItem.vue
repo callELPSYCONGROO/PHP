786b1781e4787127926573073fcc247ae1605d82
callELPSYCONGROO <mayh0991@126.com>
2018-02-09 00:39:32 +0800

    博客列表页面下拉分页（未完成）

---
+++ src/components/BlogItem.vue
@@ -0,0 +1,40 @@
+<template>
+   <div id="grey">
+      <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+          <p><img src="/static/img/user.png" width="50px" height="50px"> <span class="ba">Stanley Stinson</span></p>
+          <p><span class="bd">January 18, 2014</span></p>
+          <h4>The Amazing Spiderman</h4>
+          <p><b>Spider-Man</b> is a fictional character, a comic book superhero that appears in comic books published by Marvel Comics. Created by writer-editor Stan Lee and writer-artist Steve Ditko, he first appeared in Amazing Fantasy #15 (cover-dated Aug. 1962). </p>
+          <p>Lee and Ditko conceived the character as an orphan being raised by his Aunt May and Uncle Ben, and as a teenager, having to deal with the normal struggles of adolescence in addition to those of a costumed crimefighter.</p>
+          <p><router-link to="/MyBlogDetail">Continue Reading...</router-link></p>
+        </div>
+      </div>
+      </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "BlogItem",
+    data () {
+      return {
+        blog: {}
+      }
+    },
+    mounted: function () {
+
+    },
+    methods: {
+
+    },
+    updated: function () {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/MyBlog.vue
+++ src/pages/MyBlog.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="project-bg-color">
 
-    <div id="grey">
+    <div class="bg-grey">
       <div class="container">
       <div class="row">
         <div class="col-lg-8 col-lg-offset-2">
-          <p><img src="/static/img/user.png" width="50px" height="50px"> <ba>Stanley Stinson</ba></p>
-          <p><bd>January 18, 2014</bd></p>
+          <p><img src="/static/img/user.png" width="50px" height="50px"> <span class="ba">Stanley Stinson</span></p>
+          <p><span class="bd">January 18, 2014</span></p>
           <h4>The Amazing Spiderman</h4>
           <p><b>Spider-Man</b> is a fictional character, a comic book superhero that appears in comic books published by Marvel Comics. Created by writer-editor Stan Lee and writer-artist Steve Ditko, he first appeared in Amazing Fantasy #15 (cover-dated Aug. 1962). </p>
           <p>Lee and Ditko conceived the character as an orphan being raised by his Aunt May and Uncle Ben, and as a teenager, having to deal with the normal struggles of adolescence in addition to those of a costumed crimefighter.</p>
@@ -16,30 +16,30 @@
       </div>
     </div>
 
-    <div id="white">
+    <div class="bg-white">
       <div class="container">
       <div class="row">
         <div class="col-lg-8 col-lg-offset-2">
-          <p><img src="/static/img/user.png" width="50px" height="50px"> <ba>Stanley Stinson</ba></p>
-          <p><bd>January 3, 2014</bd></p>
+          <p><img src="/static/img/user.png" width="50px" height="50px"> <span class="ba">Stanley Stinson</span></p>
+          <p><span class="bd">January 3, 2014</span></p>
           <h4>An Image Post</h4>
           <p><img class="img-responsive" src="/static/img/blog01.jpg" alt=""></p>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-          <p><a href="/#/MyBlogDetail">Continue Reading...</a></p>
+          <p><router-link to="/MyBlogDetail">Continue Reading...</router-link></p>
         </div>
       </div>
       </div>
     </div>
-  
-    <div id="grey">
+
+    <div class="bg-grey">
       <div class="container">
       <div class="row">
         <div class="col-lg-8 col-lg-offset-2">
-          <p><img src="/static/img/user.png" width="50px" height="50px"> <ba>Stanley Stinson</ba></p>
-          <p><bd>January 01, 2014</bd></p>
+          <p><img src="/static/img/user.png" width="50px" height="50px"> <span class="ba">Stanley Stinson</span></p>
+          <p><span class="bd">January 01, 2014</span></p>
           <h4>Believe In Yourself</h4>
           <p class="bq">"A bird sitting on a tree is never afraid of the branch breaking, because her trust is not in the branch, but in her own wings."</p>
-          <p><a href="/#/MyBlogDetail">Continue Reading...</a></p>
+          <p><router-link to="/MyBlogDetail">Continue Reading...</router-link></p>
         </div>
       </div>
       </div>
@@ -49,7 +49,27 @@
 
 <script>
   export default {
-    name: 'MyBlog'
+    name: 'MyBlog',
+    mounted: function () {
+      mui.init({
+        pullRefresh : {
+          container: refreshContainer,//待刷新区域标识，querySelector能定位的css选择器均可，比如：id、.class等
+          up: {
+            height: 50,//可选.默认50.触发上拉加载拖动距离
+            auto: true,//可选,默认false.自动上拉加载一次
+            contentrefresh: '正在加载...',//可选，正在加载状态时，上拉加载控件上显示的标题内容
+            contentnomore: '没有更多数据了',//可选，请求完毕若没有更多数据时显示的提醒内容；
+            callback: upFreshFunc //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
+          }
+        }
+      });
+    },
+    methods: {
+      // 上拉刷新函数
+      upFreshFunc: function () {
+
+      }
+    }
   }
 </script>
 
